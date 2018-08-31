eee904d9538ad45a7900e9dd829c4a22dd8266ff
Brian Tobal <brian.tobal@gmail.com>
2018-08-31 19:09:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end 
 end 
 
